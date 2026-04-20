b18d6fe65b638c47b7b8f29854ff6b957dbe07fb
Victor <ghoshvictor215@gmail.com>
2026-04-19 21:41:52 -0400

    trying to fix logging

--- backend/clearerr.py
+++ backend/clearerr.py
@@ -14,25 +14,27 @@ from settings.config import config
 
 # region 0: Logging setup
 log_path = Path(config._LOG_PATH)
-
 if not log_path.parent.exists():
     log_path.parent.mkdir(parents=True, exist_ok=True)
 
 max_bytes = config.LOG_SIZE_MB * 1024 * 1024
-
 log_level = getattr(logging, config.LOG_LEVEL)
-
 formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-file_handler = RotatingFileHandler(config._LOG_PATH, maxBytes=max_bytes, backupCount=3)
-file_handler.setFormatter(formatter)
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
 
-logging.getLogger().setLevel(log_level)
-logging.getLogger().addHandler(file_handler)
-logging.getLogger().addHandler(stream_handler)
+file_handler = RotatingFileHandler(log_path, maxBytes=max_bytes, backupCount=3)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(log_level)
+root_logger.addHandler(file_handler)
+root_logger.addHandler(console_handler)
+
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("requests").setLevel(logging.WARNING)
+
 log = logging.getLogger(__name__)
 
 log.info("Required pyhton libraries loaded")
