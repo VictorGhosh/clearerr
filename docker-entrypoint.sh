f24f129bcb16b8167e769b4c187beec4e3068fd4
Victor <ghoshvictor215@gmail.com>
2026-04-20 09:01:43 -0400

    added scheduling3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 python3 -m uvicorn frontend.main:app --host 0.0.0.0 --port 8000 &
 MAIN_PROCESS_PID=$!
 
-echo "0 */${RUN_FREQUENCY:-12} * * * python3 -m backend.clearerr > /proc/1/fd/1 2>&1" | crontab -
+echo "${BACKEND_CRON:-0 0 * * *} python3 -m backend.clearerr > /proc/1/fd/1 2>&1" | crontab -
 
 # cron in background
 service cron start
